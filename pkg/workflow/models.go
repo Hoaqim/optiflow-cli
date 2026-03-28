09232812010223c2a11249281364b673a4cb041e
Hoaqim <michaldomasik@wp.pl>
2026-03-28 17:15:47 +0100

    [Optimization] Added more structs for yaml parser

--- pkg/workflow/models.go
+++ pkg/workflow/models.go
@@ -1,21 +1,40 @@
 package workflow
 
 type Workflow struct {
-	Name string         `yaml:"name"`
-	On   any            `yaml:"on"` // Can be a list, string, or map
-	Jobs map[string]Job `yaml:"jobs"`
+	Name        string            `yaml:"name,omitempty"`
+	RunName     string            `yaml:"run-name,omitempty"`
+	On          any               `yaml:"on"` // Can be string, array, or map
+	Env         map[string]string `yaml:"env,omitempty"`
+	Permissions map[string]string `yaml:"permissions,omitempty"`
+	Concurrency any               `yaml:"concurrency,omitempty"` // Can be string or map
+	Jobs        map[string]Job    `yaml:"jobs"`
 }
 
 type Job struct {
-	RunsOn  string   `yaml:"runs-on"`
-	Needs   []string `yaml:"needs,omitempty"` //Can be a single string or list
-	Timeout int      `yaml:"timeout-minutes,omitempty"`
-	Steps   []Step   `yaml:"steps"`
+	Name           string            `yaml:"name,omitempty"`
+	Permissions    map[string]string `yaml:"permissions,omitempty"`
+	Needs          any               `yaml:"needs,omitempty"` // Can be string or []string
+	If             string            `yaml:"if,omitempty"`
+	RunsOn         any               `yaml:"runs-on"` // Can be string or []string (e.g., matrix)
+	Environment    any               `yaml:"environment,omitempty"`
+	Concurrency    any               `yaml:"concurrency,omitempty"`
+	Outputs        map[string]string `yaml:"outputs,omitempty"`
+	Env            map[string]string `yaml:"env,omitempty"`
+	TimeoutMinutes float64           `yaml:"timeout-minutes,omitempty"`
+	ContinueError  any               `yaml:"continue-on-error,omitempty"`
+	Steps          []Step            `yaml:"steps"`
 }
 
 type Step struct {
-	Name string            `yaml:"name,omitempty"`
-	Uses string            `yaml:"uses,omitempty"`
-	Run  string            `yaml:"run,omitempty"`
-	Env  map[string]string `yaml:"env,omitempty"`
+	Id               string            `yaml:"id,omitempty"`
+	If               string            `yaml:"if,omitempty"`
+	Name             string            `yaml:"name,omitempty"`
+	Uses             string            `yaml:"uses,omitempty"`
+	Run              string            `yaml:"run,omitempty"`
+	WorkingDirectory string            `yaml:"working-directory,omitempty"`
+	Shell            string            `yaml:"shell,omitempty"`
+	With             map[string]any    `yaml:"with,omitempty"`
+	Env              map[string]string `yaml:"env,omitempty"`
+	ContinueError    any               `yaml:"continue-on-error,omitempty"`
+	TimeoutMinutes   float64           `yaml:"timeout-minutes,omitempty"`
 }
